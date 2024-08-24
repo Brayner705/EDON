9fcfdba4555a57930199b73698c91fdbe0124cf0
Brayner705 <braynerbracho.fcb@gmail.com>
2024-08-24 23:29:24 +0200

    Tarjeta de carrera

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MenuNavegacionComponent} from './menu-navegacion/menu-navegacion.component';
+import {CarrerasComponent} from './carreras/carreras.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuNavegacionComponent, CarrerasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EDON';
-  loggin = false;
 }
